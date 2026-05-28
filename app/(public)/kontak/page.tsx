4348ff58342f993b705e218affb18df6aaec19ee
RaditSugara02 <raditsugarawd@gmail.com>
2026-05-28 22:00:31 +0700

    fix(kontak): fix Google Maps embed Place ID to show cathedral pin correctly

--- app/(public)/kontak/page.tsx
+++ app/(public)/kontak/page.tsx
@@ -131,7 +131,7 @@ export default async function KontakPage() {
               </div>
               <div className="relative w-full h-full min-h-96">
                 <iframe
-                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d993.7633700785!2d109.33882837573483!3d-0.02657003610827697!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f17!3m3!1m2!1s0x2e1d5905b3e6af71%3A0x2a7d76d43cfdf52e!2sKatedral%20Santo%20Yosef%20Pontianak!5e0!3m2!1sid!2sid!4v1715778000000!5m2!1sid!2sid"
+                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d993.7633700785!2d109.33882837573483!3d-0.02657003610827697!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f17!3m3!1m2!1s0x2e1d585096004cfb%3A0x970b0a544ceba22f!2sGereja%20Katedral%20Pontianak%2C%20Santo%20Yosef!5e0!3m2!1sid!2sid!4v1748440000000!5m2!1sid!2sid"
                   width="100%"
                   height="100%"
                   className="absolute inset-0 w-full h-full border-0"
